0830ca7c22a7efae23444d87ebc35e70b881796b
drichards188 <github@cxoprotect.com>
2022-10-06 08:27:19 -0600

    installed prettier formatter closing #17

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,30 +1,37 @@
-import {useAppDispatch, useAppSelector} from "../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import Header from "../../etc/Header";
-import {Cards} from "./Cards"
-import {useEffect} from "react";
-import {getCoinBatchAsync, getCoinDataAsync, getCoinListAsync, selectCoinData} from "./dashboardSlice";
+import { Cards } from "./Cards";
+import { useEffect } from "react";
+import {
+  getCoinBatchAsync,
+  getCoinDataAsync,
+  getCoinListAsync,
+  selectCoinData,
+} from "./dashboardSlice";
 import SearchBar from "./SearchBar";
 import SearchAppBar from "./Search";
 
 export function Dashboard() {
-    const dispatch = useAppDispatch();
-    const coinData = useAppSelector(selectCoinData);
-    const coinKey = 'ripple';
+  const dispatch = useAppDispatch();
+  const coinData = useAppSelector(selectCoinData);
+  const coinKey = "ripple";
 
-    useEffect(() => {
-        dispatch(getCoinBatchAsync({
-            coinArray: ['ripple', 'bitcoin']
-        }));
-        dispatch(getCoinListAsync('placeholder payload'));
-    }, []);
-
-    return (
-        <div>
-            {/*<Header/>*/}
-            {/*<p>This is the last price of {coinKey} ${coinData[0].last}</p>*/}
-            {/*<SearchBar/>*/}
-            <SearchAppBar />
-            <Cards cardData={coinData}/>
-        </div>
+  useEffect(() => {
+    dispatch(
+      getCoinBatchAsync({
+        coinArray: ["ripple", "bitcoin"],
+      })
     );
+    dispatch(getCoinListAsync("placeholder payload"));
+  }, []);
+
+  return (
+    <div>
+      {/*<Header/>*/}
+      {/*<p>This is the last price of {coinKey} ${coinData[0].last}</p>*/}
+      {/*<SearchBar/>*/}
+      <SearchAppBar />
+      <Cards cardData={coinData} />
+    </div>
+  );
 }
