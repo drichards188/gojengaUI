443a06dafff688fffc2e635716176e3e986670f0
drichards188 <github@cxoprotect.com>
2023-01-05 12:41:11 -0700

    rounding card data to two decimal places

--- src/components/dashboard/Card.tsx
+++ src/components/dashboard/Card.tsx
@@ -1,31 +1,33 @@
-import React, {useContext} from 'react';
-import ReactDOM from 'react-dom';
-import {useState, createContext} from "react";
+import React, { useContext } from "react";
+import ReactDOM from "react-dom";
+import { useState, createContext } from "react";
 
 const Card = (props: any) => {
+  let { id, last, volume } = props.data;
 
-    const {id, last, volume}= props.data;
+  last = Math.round(last * 100) / 100;
+  volume = Math.round(volume * 100) / 100;
 
-    const divStyle = {
-        display: 'inline-block',
-        marginRight: '25px',
-        padding: '10px',
-        backgroundColor: '#4d535c',
-        color: '#4fc3f7'
-    }
+  const divStyle = {
+    display: "inline-block",
+    marginRight: "25px",
+    padding: "10px",
+    backgroundColor: "#4d535c",
+    color: "#4fc3f7",
+  };
 
-    return (
+  return (
     <div style={divStyle}>
-        <p>{id}</p>
-        <p>{last}</p>
-        <p>{volume}</p>
-        {/*<ul>*/}
-        {/*    {props.data.stops.map(myStops => (*/}
-        {/*        <li>{myStops}</li>*/}
-        {/*    ))}*/}
-        {/*</ul>*/}
+      <p>{id}</p>
+      <p>{last}</p>
+      <p>{volume}</p>
+      {/*<ul>*/}
+      {/*    {props.data.stops.map(myStops => (*/}
+      {/*        <li>{myStops}</li>*/}
+      {/*    ))}*/}
+      {/*</ul>*/}
     </div>
-    )
-}
+  );
+};
 
 export default Card;
