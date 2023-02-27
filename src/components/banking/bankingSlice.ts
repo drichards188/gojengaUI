b58ae704c9ebc6a666e215db5c819c52ca5d195a
David Richards <50757879+drichards188@users.noreply.github.com>
2023-02-27 15:19:19 -0700

    Error handling (#41)
    * added proper error handling to logging in
    
    * added proper error handling to logging in
    
    * cleaned up unused assets

--- src/components/banking/bankingSlice.ts
+++ src/components/banking/bankingSlice.ts
@@ -44,15 +44,6 @@ const initialState: BankingState = {
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched. Thunks are
 // typically used to make async requests.
-export const incrementAsync = createAsyncThunk(
-  "banking/incrementAsync",
-  async (amount: number) => {
-    const response = await fetchCount(amount);
-    // The value we return becomes the `fulfilled` action payload
-
-    return response.data;
-  }
-);
 
 export const createUserAsync = createAsyncThunk(
   "banking/createUser",
@@ -129,25 +120,14 @@ export const bankingSlice = createSlice({
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.amount += 1;
-    },
-    decrement: (state) => {
-      state.amount -= 1;
-    },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.amount += action.payload;
-    },
     setAmount: (state, action: PayloadAction<number>) => {
       state.amount = action.payload;
     },
     setToken: (state, action: PayloadAction<string>) => {
       state.token = action.payload;
+    },
+    setLoggedIn: (state, action: PayloadAction<boolean>) => {
+      state.loggedIn = action.payload;
     },
     resetState: (state) => {
       return initialState;
@@ -276,9 +256,6 @@ export const bankingSlice = createSlice({
 });
 
 export const {
-  increment,
-  decrement,
-  incrementByAmount,
   setAmount,
   resetState,
   resetMessage,
@@ -289,6 +266,8 @@ export const {
   makeDelete,
   setUser,
   setToken,
+  setLoggedIn,
+
 } = bankingSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
@@ -301,16 +280,5 @@ export const selectToken = (state: RootState) => state.banking.token;
 export const selectMessage = (state: RootState) => state.banking.message;
 export const selectBalance = (state: RootState) => state.banking.balance;
 export const selectAmount = (state: RootState) => state.banking.amount;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd =
-  (amount: number): AppThunk =>
-  (dispatch, getState) => {
-    const currentValue = selectBanking(getState());
-    if (currentValue % 2 === 1) {
-      dispatch(incrementByAmount(amount));
-    }
-  };
 
 export default bankingSlice.reducer;
