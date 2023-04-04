fcd3964700ccd3c125fe71c3b4ff64485cc6fde7
David Richards <50757879+drichards188@users.noreply.github.com>
2023-04-04 09:42:21 -0600

    Pagination to search (#56)
    * rough pagination added to search
    
    * pagination working

--- src/components/dashboard/List.tsx
+++ src/components/dashboard/List.tsx
@@ -1,9 +1,14 @@
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { selectCoinList, addCoinToDisplayList } from "./dashboardSlice";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import CustomButton from "../general/CustomButton";
 
 function List(props: any) {
   const coinData = useAppSelector(selectCoinList);
+
+  const [searchMin, setSearchMin] = useState(0);
+  const [searchMax, setSearchMax] = useState(10);
 
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
@@ -35,10 +40,9 @@ function List(props: any) {
       "0px 3px 1px -2px rgba(112,76,182),0px 2px 2px 0px rgba(112,76,182,0.9),0px 1px 5px 0px rgba(82,0,130,0.12)",
   };
 
-  //todo add pagination to results
   return (
     <ul>
-      {filteredData.slice(0, 10).map((item: any) => (
+      {filteredData.slice(searchMin, searchMax).map((item: any) => (
         <li
           style={divStyle}
           onClick={() => {
@@ -49,6 +53,29 @@ function List(props: any) {
           {item.id}
         </li>
       ))}
+      {props.input && (
+        <CustomButton
+          label={"Less"}
+          clickFunction={() => {
+            if (searchMin >= 10) {
+              setSearchMin(searchMin - 10);
+              setSearchMax(searchMax - 10);
+            }
+          }}
+        />
+      )}
+
+      {props.input && (
+        <CustomButton
+          label={"More"}
+          clickFunction={() => {
+            if (searchMax + 10 <= coinData.length) {
+              setSearchMin(searchMin + 10);
+              setSearchMax(searchMax + 10);
+            }
+          }}
+        />
+      )}
     </ul>
   );
 }
