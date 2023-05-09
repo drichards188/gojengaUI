3231189870fdbf5e2b968f7b3547d66aeabf56e3
drichards188 <github@cxoprotect.com>
2023-05-09 11:57:56 -0600

    token service and interceptor integrated

---
+++ src/services/token.service.ts
@@ -0,0 +1,40 @@
+class TokenService {
+  getLocalRefreshToken() {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const userObj = JSON.parse(user);
+      return userObj?.refreshToken;
+    }
+  }
+
+  getLocalAccessToken() {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const userObj = JSON.parse(user);
+      return userObj?.accessToken;
+    }
+  }
+
+  updateLocalAccessToken(token: any) {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const userObj = JSON.parse(user);
+      localStorage.setItem("user", JSON.stringify(user));
+    }
+  }
+
+  // getUser() {
+  //   return JSON.parse(localStorage.getItem("user"));
+  // }
+  //
+  // setUser(user) {
+  //   console.log(JSON.stringify(user));
+  //   localStorage.setItem("user", JSON.stringify(user));
+  // }
+
+  removeUser() {
+    localStorage.removeItem("user");
+  }
+}
+
+export default new TokenService();
---
+++ src/setupInterceptors.ts
@@ -0,0 +1,56 @@
+import axiosInstance from "./axiosInstance";
+import TokenService from "./services/token.service";
+import { setToken } from "./components/banking/bankingSlice";
+
+const setup = (store: { dispatch: any }) => {
+  axiosInstance.interceptors.request.use(
+    (config) => {
+      const token = TokenService.getLocalAccessToken();
+      if (token) {
+        // config.headers["Authorization"] = 'Bearer ' + token;  // for Spring Boot back-end
+        // @ts-ignore
+        config.headers["x-access-token"] = token; // for Node.js Express back-end
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const { dispatch } = store;
+  axiosInstance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (err) => {
+      const originalConfig = err.config;
+
+      if (originalConfig.url !== "/auth/signin" && err.response) {
+        // Access Token was expired
+        if (err.response.status === 401 && !originalConfig._retry) {
+          originalConfig._retry = true;
+
+          try {
+            const rs = await axiosInstance.post("/auth/refreshtoken", {
+              refreshToken: TokenService.getLocalRefreshToken(),
+            });
+
+            const { accessToken } = rs.data;
+
+            dispatch(setToken(accessToken));
+            TokenService.updateLocalAccessToken(accessToken);
+
+            return axiosInstance(originalConfig);
+          } catch (_error) {
+            return Promise.reject(_error);
+          }
+        }
+      }
+
+      return Promise.reject(err);
+    }
+  );
+};
+
+export default setup;
