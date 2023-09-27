11fdf4c93b1ffbc241a43c6ab048ea9e1d33bfae
drichards188 <github@cxoprotect.com>
2023-09-26 18:20:15 -0600

    new currency input works!

--- src/components/deposit/Deposit.tsx
+++ src/components/deposit/Deposit.tsx
@@ -11,7 +11,6 @@ import {
 } from "../banking/bankingSlice";
 import styles from "../banking/Banking.module.css";
 // @ts-ignore
-import NumberFormat from "react-number-format";
 import {
   Box,
   FormControl,
@@ -22,6 +21,7 @@ import {
   TextField,
 } from "@mui/material";
 import { BankComponents } from "../banking/Banking";
+import { NumericFormat } from "react-number-format";
 import CurrencyInput from "../general/CurrencyInput";
 
 let USDollar = new Intl.NumberFormat("en-US", {
@@ -35,50 +35,55 @@ export function Deposit(props: any) {
   const jwtToken = useAppSelector(selectToken);
 
   const dispatch = useAppDispatch();
-  const [amount, setStateAmount] = useState("0");
+  const [amount, setStateAmount] = useState(0);
+  const [numberValue, setNumberValue] = useState(0);
   const amountValue = Number(amount) || 0;
-  const formattedAmount: string = USDollar.format(amountValue);
-
-  const formatAmount = (amount: any) => {
-    const cleanedString: string = amount.replace(/[^0-9.]/g, "");
-
-    const numberValue: number = parseFloat(cleanedString);
-    setStateAmount(String(numberValue));
-  };
 
   // @ts-ignore
   let createDepositElem = (
     <Grid container className={styles.row}>
       <div>
-        <TextField
-          id="deposit-amount"
-          label="Deposit Amount"
-          variant="standard"
-          inputMode="numeric"
-          autoFocus={true}
-          className={styles.textbox}
-          aria-label="Deposit Amount"
-          value={formattedAmount}
-          // InputProps={{
-          //   startAdornment: <span>$</span>, // you could also use the InputAdornment component from MUI here
-          // }}
-          sx={{
-            "& .MuiInputBase-root": {
-              color: "primary.main",
-            },
-            "& .MuiFormLabel-root": {
-              color: "secondary.main",
-            },
-            "& .MuiFormLabel-root.Mui-focused": {
-              color: "primary.main",
-            },
-          }}
-          onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
-            formatAmount(e.target.value)
-          }
-        />
+        {/*<TextField*/}
+        {/*  id="deposit-amount"*/}
+        {/*  label="Deposit Amount"*/}
+        {/*  variant="standard"*/}
+        {/*  inputMode="numeric"*/}
+        {/*  autoFocus={true}*/}
+        {/*  className={styles.textbox}*/}
+        {/*  aria-label="Deposit Amount"*/}
+        {/*  value={formattedAmount}*/}
+        {/*  // InputProps={{*/}
+        {/*  //   startAdornment: <span>$</span>, // you could also use the InputAdornment component from MUI here*/}
+        {/*  // }}*/}
+        {/*  sx={{*/}
+        {/*    "& .MuiInputBase-root": {*/}
+        {/*      color: "primary.main",*/}
+        {/*    },*/}
+        {/*    "& .MuiFormLabel-root": {*/}
+        {/*      color: "secondary.main",*/}
+        {/*    },*/}
+        {/*    "& .MuiFormLabel-root.Mui-focused": {*/}
+        {/*      color: "primary.main",*/}
+        {/*    },*/}
+        {/*  }}*/}
+        {/*  onChange={(e: { target: { value: React.SetStateAction<string> } }) =>*/}
+        {/*    formatAmount(e.target.value)*/}
+        {/*  }*/}
+        {/*/>*/}
 
-        <CurrencyInput placeholder="$0.00" type="text" />
+        <NumericFormat
+          prefix={"$"}
+          defaultValue={0}
+          thousandSeparator={true}
+          value={numberValue}
+          allowNegative={false}
+          decimalScale={2}
+          customInput={CurrencyInput}
+          onValueChange={(v: any) => {
+            setNumberValue(v.value);
+          }}
+          valueCallback={setNumberValue}
+        />
       </div>
 
       {/*<NumberFormat*/}
@@ -92,11 +97,10 @@ export function Deposit(props: any) {
       <button
         className={styles.button}
         onClick={() => {
-          const floatDepositAmount = parseFloat(amount);
           dispatch(
             createDepositAsync({
               account: bankingUser,
-              amount: floatDepositAmount,
+              amount: numberValue,
               jwt: jwtToken,
             })
           );
--- src/components/general/CurrencyInput.tsx
+++ src/components/general/CurrencyInput.tsx
@@ -1,58 +1,42 @@
+import styles from "../banking/Banking.module.css";
 import React from "react";
-import PropTypes from "prop-types";
-import MaskedInput from "react-text-mask";
-import createNumberMask from "text-mask-addons/dist/createNumberMask";
+import { TextField } from "@mui/material";
 
-const defaultMaskOptions = {
-  prefix: "$",
-  suffix: "",
-  includeThousandsSeparator: true,
-  thousandsSeparatorSymbol: ",",
-  allowDecimal: true,
-  decimalSymbol: ".",
-  decimalLimit: 2, // how many digits allowed after the decimal
-  integerLimit: 7, // limit length of integer numbers
-  allowNegative: false,
-  allowLeadingZeroes: false,
+const CurrencyInput = (props: any) => {
+  // alert(JSON.stringify(props));
+
+  const [formattedAmount, setFormattedAmount] = React.useState("");
+
+  return (
+    <TextField
+      id="deposit-amount"
+      label="Deposit Amount"
+      type={props.type}
+      variant="standard"
+      inputMode={props.inputMode}
+      autoFocus={true}
+      className={styles.textbox}
+      aria-label="Deposit Amount"
+      value={props.value}
+      onChange={(e: { target: { value: React.SetStateAction<string> } }) => {
+        props.valueCallback(e.target.value);
+      }}
+      // InputProps={{
+      //   startAdornment: <span>$</span>, // you could also use the InputAdornment component from MUI here
+      // }}
+      sx={{
+        "& .MuiInputBase-root": {
+          color: "primary.main",
+        },
+        "& .MuiFormLabel-root": {
+          color: "secondary.main",
+        },
+        "& .MuiFormLabel-root.Mui-focused": {
+          color: "primary.main",
+        },
+      }}
+    />
+  );
 };
-
-// const CurrencyInput = ({ maskOptions, ...inputProps }) => {
-//   const currencyMask = createNumberMask({
-//     ...defaultMaskOptions,
-//     ...maskOptions,
-//   });
-//
-//   return <MaskedInput mask={currencyMask} {...inputProps} />;
-// };
-
-const CurrencyInput = ({ ...inputProps }) => {
-  const currencyMask = createNumberMask({
-    ...defaultMaskOptions,
-  });
-
-  return <MaskedInput mask={currencyMask} {...inputProps} />;
-};
-
-CurrencyInput.defaultProps = {
-  inputMode: "numeric",
-  maskOptions: {},
-};
-
-// CurrencyInput.propTypes = {
-//   inputmode: PropTypes.string,
-//   maskOptions: PropTypes.shape({
-//     prefix: PropTypes.string,
-//     suffix: PropTypes.string,
-//     includeThousandsSeparator: PropTypes.boolean,
-//     thousandsSeparatorSymbol: PropTypes.string,
-//     allowDecimal: PropTypes.boolean,
-//     decimalSymbol: PropTypes.string,
-//     decimalLimit: PropTypes.string,
-//     requireDecimal: PropTypes.boolean,
-//     allowNegative: PropTypes.boolean,
-//     allowLeadingZeroes: PropTypes.boolean,
-//     integerLimit: PropTypes.number,
-//   }),
-// };
 
 export default CurrencyInput;
