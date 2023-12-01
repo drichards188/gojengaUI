a44e956034988efb1ccd5d3648cf2ce3ae8f7ded
drichards188 <github@cxoprotect.com>
2023-12-01 09:17:46 -0700

    changed layout and added hard coded alpha and beta

--- src/components/risk/Risk.tsx
+++ src/components/risk/Risk.tsx
@@ -29,8 +29,10 @@ const Risk = () => {
     async function getAllSymbols() {
       let response = await getCalcSymbols(jwtToken);
       if (response) {
-        setSecurityList([...securityList, ...response.data]);
-        alert(`securityList ${securityList}`);
+        if (!securityList.includes(response.data[0])) {
+          setSecurityList([...securityList, ...response.data]);
+        }
+        // alert(`securityList ${securityList}`);
       }
     }
 
@@ -43,56 +45,86 @@ const Risk = () => {
         <Header />
       </Grid>
 
-      <Grid item sm={12} md={10}>
-        <Paper>
-          <Grid item sm={4}>
-            <h1>Risk</h1>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={securityList}
-              sx={{
-                "& .MuiInputBase-root": {
-                  color: "primary.main",
-                },
-                "& .MuiFormLabel-root": {
-                  color: "secondary.main",
-                },
-                "& .MuiFormLabel-root.Mui-focused": {
-                  color: "primary.main",
-                },
-                width: 300,
-              }}
-              value={securitySymbol}
-              inputValue={inputValue}
-              onInputChange={(event, newInputValue) => {
-                if (newInputValue !== "Symbol") {
-                  setInputValue(newInputValue);
-                  setShowTv(true);
-                } else {
-                  // setInputValue("Symbol");
-                  setShowTv(false);
-                }
-              }}
-              onChange={(event: any, newValue: string | null) => {
-                if (newValue !== "Symbol") {
-                  setSecuritySymbol(newValue);
-                  setShowTv(true);
-                } else {
-                  // setSecuritySymbol("Symbol");
-                  setShowTv(false);
-                }
-              }}
-              renderInput={(params) => (
-                <TextField {...params} label="Security Symbol" />
-              )}
-            />
+      <Grid item sm={12} md={10} style={{ backgroundColor: "rgba(0,0,0,.2)" }}>
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          <Grid item sm={12} md={6}>
+            <Paper>
+              <Grid
+                container
+                spacing={2}
+                justifyContent="center"
+                alignItems="space-evenly"
+              >
+                <Grid item sm={4}>
+                  <h1>Risk</h1>
+                </Grid>
+                <Grid item sm={6}>
+                  <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={securityList}
+                    sx={{
+                      "& .MuiInputBase-root": {
+                        color: "primary.main",
+                      },
+                      "& .MuiFormLabel-root": {
+                        color: "secondary.main",
+                      },
+                      "& .MuiFormLabel-root.Mui-focused": {
+                        color: "primary.main",
+                      },
+                      width: 300,
+                    }}
+                    value={securitySymbol}
+                    inputValue={inputValue}
+                    onInputChange={(event, newInputValue) => {
+                      if (newInputValue !== "Symbol") {
+                        setInputValue(newInputValue);
+                        setShowTv(true);
+                      } else {
+                        // setInputValue("Symbol");
+                        setShowTv(false);
+                      }
+                    }}
+                    onChange={(event: any, newValue: string | null) => {
+                      if (newValue !== "Symbol") {
+                        setSecuritySymbol(newValue);
+                        setShowTv(true);
+                      } else {
+                        // setSecuritySymbol("Symbol");
+                        setShowTv(false);
+                      }
+                    }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Security Symbol" />
+                    )}
+                  />
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
+        </Grid>
 
+        <Paper>
           {showTv && (
             <div>
-              <Grid item sm={4}>
-                <SharpeRatio symbol={securitySymbol} />
+              <Grid item sm={12}>
+                <h2>Calculations</h2>
+                <Grid container spacing={2} alignItems="space-between">
+                  <Grid item sm={4}>
+                    <SharpeRatio symbol={securitySymbol} />
+                  </Grid>
+                  <Grid item sm={4}>
+                    <p>Alpha</p>
+                    <p>1.7%</p>
+                    <p>Good</p>
+                  </Grid>
+                  <Grid item sm={4}>
+                    <p>Beta</p>
+                    <p>1.1</p>
+                    <p>Ok</p>
+                  </Grid>
+                </Grid>
               </Grid>
               <Grid item sm={12} style={{ height: "40vh" }}>
                 <TradingViewWidget symbol={securitySymbol} />
