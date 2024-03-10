80dbc3785856570b9176dfee4b753b74f59d4fc4
drichards188 <github@cxoprotect.com>
2024-03-10 07:22:38 -0600

    added two more tests to risk page

--- src/components/risk/Risk.tsx
+++ src/components/risk/Risk.tsx
@@ -126,6 +126,7 @@ const Risk = () => {
                     )}
                   />
                   <Button
+                    id="risk-retrieve"
                     onClick={() => {
                       setSecuritySymbol(inputValue);
                       setShowTv(true);
