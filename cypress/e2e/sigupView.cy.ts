dfb5f5fb0705337c9fb7e6c0e5efbcc68cf2463d
drichards188 <github@cxoprotect.com>
2024-03-14 07:59:55 -0700

    updating signup test

--- cypress/e2e/sigupView.cy.ts
+++ cypress/e2e/sigupView.cy.ts
@@ -8,11 +8,4 @@ describe("template spec", () => {
         cy.get('h1').contains("Signup");
     });
 
-    it("should create account with demo creds", () => {
-        cy.get("[data-cy='username-field']").type("hire");
-        cy.get("[data-cy='password-field']").type("me");
-        cy.get("[data-cy='login-button']").click();
-        cy.get("a").contains("Portfolio");
-        cy.contains("Offline Demo Mode");
-    });
 });
